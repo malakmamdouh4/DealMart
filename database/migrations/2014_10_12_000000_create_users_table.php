842bd7ee0a9578e3e96eb2c7f1a200c05855c141
malakmamdouh4 <mammalak789@gmail.com>
2022-06-08 20:47:35 +0200

    user status

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('status');
+            $table->integer('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
